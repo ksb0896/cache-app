741b6a4be7400d4d67160a20d915d10ee77c26bb
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-07-28 23:33:46 +0530

    working skeleton --done

---
+++ src/test/java/com/ksb/cache/CacheAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksb.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
