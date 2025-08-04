1a06c7ae9a01c5f52e90bd1e6a994d83c03efc39
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-08-04 22:42:28 +0530

    enabled specify key expiration time(TTL)

--- src/main/java/com/ksb/cache/config/RedisConfig.java
+++ src/main/java/com/ksb/cache/config/RedisConfig.java
@@ -3,10 +3,15 @@ package com.ksb.cache.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -26,5 +31,23 @@ public class RedisConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         return RedisCacheManager.create(connectionFactory);
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager() {
+        RedisCacheConfiguration cacheConfig = myDefaultCacheConfig(Duration.ofMinutes(10)).disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory())
+                .cacheDefaults(cacheConfig)
+                .withCacheConfiguration("tutorials", myDefaultCacheConfig(Duration.ofMinutes(5)))
+                .withCacheConfiguration("tutorial", myDefaultCacheConfig(Duration.ofMinutes(1)))
+                .build();
+    }
+
+    private RedisCacheConfiguration myDefaultCacheConfig(Duration duration) {
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(duration)
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
 }
