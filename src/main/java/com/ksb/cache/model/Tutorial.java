595ecd884624d58d4dc9fb946ba5951743528ac1
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-08-11 19:23:52 +0530

    models created

--- src/main/java/com/ksb/cache/model/Tutorial.java
+++ src/main/java/com/ksb/cache/model/Tutorial.java
@@ -1,4 +1,33 @@
 package com.ksb.cache.model;
 
-public class Tutorial {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "tutorials")
+public class Tutorial implements Serializable {
+
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private String published;
+
+    @Override
+    public String toString(){
+        return "Tutorial [id" + id + ", title=" + ", desc=" + description + ", published=" + published + "]";
+    }
+
 }
--- src/main/java/com/ksb/cache/repo/TutorialRepository.java
+++ src/main/java/com/ksb/cache/repo/TutorialRepository.java
@@ -1,4 +1,14 @@
 package com.ksb.cache.repo;
 
-public interface TutorialRepository {
+import com.ksb.cache.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
 }
